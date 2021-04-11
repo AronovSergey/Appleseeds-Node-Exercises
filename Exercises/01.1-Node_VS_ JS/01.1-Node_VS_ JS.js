78e3621a06fa94ead48fa1c1340094ae9b75e0fc
Sergey <aronovsergeyy@gmail.com>
2021-04-11 17:08:43 +0300

    1 Submission

---
+++ Exercises/01.1-Node_VS_ JS/01.1-Node_VS_ JS.js
@@ -0,0 +1,17 @@
+/*
+
+What are 3 things node.js has that the client side javascript doesn’t
+have. And the reasons why.
+1.global object
+2.process object
+3.run outside the browser.
+4.Nodejs is written in C, C++ and Javascript.
+
+What are 3 things the client side javascript has that node.js doesn’t
+have. And the reasons why
+1.window object
+2.document object
+3.Javascript can only be run in the browsers
+4.It is the upgraded version of ECMA script that uses Chrome’s V8 engine written in C++
+
+*/
