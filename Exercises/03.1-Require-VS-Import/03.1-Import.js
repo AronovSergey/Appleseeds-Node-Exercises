37bc06fcf4e3870fd513896c15fe162737a36cf0
Sergey <aronovsergeyy@gmail.com>
2021-04-12 00:16:35 +0300

    3 Submission

---
+++ Exercises/03.1-Require-VS-Import/03.1-Import.js
@@ -0,0 +1,13 @@
+const sum = (a, b) => {
+    return a + b;
+}
+
+const minus = (a, b) => {
+    return a - b;
+}
+
+const mult = (a, b) => {
+    return a * b;
+}
+
+module.exports = { sum, minus, mult }
---
+++ Exercises/03.1-Require-VS-Import/03.1-Require-VS-Import.js
@@ -0,0 +1,5 @@
+const { sum, minus, mult } = require("./03.1-Import");
+
+console.log(sum(1, 2));
+console.log(minus(3, 1));
+console.log(mult(3, 2));
