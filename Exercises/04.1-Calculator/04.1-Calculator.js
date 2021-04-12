6f504f0731496de45b75812e1e846e94386d0164
Sergey <aronovsergeyy@gmail.com>
2021-04-12 22:38:34 +0300

    Express Example

--- Exercises/04.1-Calculator/04.1-Calculator.js
+++ Exercises/04.1-Calculator/04.1-Calculator.js
@@ -1,82 +1,81 @@
-const yargs = require('yargs');
+const yargs = require("yargs");
 
 // Creat add command
 yargs.command({
-    command: 'add',
-    describe: 'add is defined as the result we get on adding two numbers',
-    builder: {
-        x: {
-            describe: 'First Number',
-            demandOption: true,
-            type: 'number',
-        },
-        y: {
-            describe: 'Second Number',
-            demandOption: true,
-            type: 'number',
-        },
+  command: "add",
+  describe: "add is defined as the result we get on adding two numbers",
+  builder: {
+    x: {
+      describe: "First Number",
+      demandOption: true,
+      type: "number",
     },
-    handler: function(argv) {
-        console.log(`The result of ${argv.x} + ${argv.y} is ${argv.x + argv.y}`);
-    }
+    y: {
+      describe: "Second Number",
+      demandOption: true,
+      type: "number",
+    },
+  },
+  handler: function (argv) {
+    console.log(`The result of ${argv.x} + ${argv.y} is ${argv.x + argv.y}`);
+  },
 });
-
 
 // Creat sub command
 yargs.command({
-    command: 'sub',
-    describe: 'sub is defined as the result we get on subtraction of two numbers',
-    builder: {
-        x: {
-            describe: 'First Number',
-            demandOption: true,
-            type: 'number',
-        },
-        y: {
-            describe: 'Second Number',
-            demandOption: true,
-            type: 'number',
-        },
+  command: "sub",
+  describe: "sub is defined as the result we get on subtraction of two numbers",
+  builder: {
+    x: {
+      describe: "First Number",
+      demandOption: true,
+      type: "number",
     },
-    handler: function(argv) {
-        console.log(`The result of ${argv.x} - ${argv.y} is ${argv.x - argv.y}`);
-    }
-});
-
-
-yargs.command({
-    command: 'mult',
-    describe: 'mult is defined as the result we get on multiplication of two numbers',
-    builder: {
-        x: {
-            describe: 'First Number',
-            demandOption: true,
-            type: 'number',
-        },
-        y: {
-            describe: 'Second Number',
-            demandOption: true,
-            type: 'number',
-        },
+    y: {
+      describe: "Second Number",
+      demandOption: true,
+      type: "number",
     },
-    handler: function(argv) {
-        console.log(`The result of ${argv.x} * ${argv.y} is ${argv.x * argv.y}`);
-    }
+  },
+  handler: function (argv) {
+    console.log(`The result of ${argv.x} - ${argv.y} is ${argv.x - argv.y}`);
+  },
 });
 
 yargs.command({
-    command: 'pow',
-    describe: 'pow is defined as the result we get on squering some number',
-    builder: {
-        x: {
-            describe: 'First Number',
-            demandOption: true,
-            type: 'number',
-        },
+  command: "mult",
+  describe:
+    "mult is defined as the result we get on multiplication of two numbers",
+  builder: {
+    x: {
+      describe: "First Number",
+      demandOption: true,
+      type: "number",
     },
-    handler: function(argv) {
-        console.log(`The result of ${argv.x} ^ 2 is ${argv.x * argv.x}`);
-    }
+    y: {
+      describe: "Second Number",
+      demandOption: true,
+      type: "number",
+    },
+  },
+  handler: function (argv) {
+    console.log(`The result of ${argv.x} * ${argv.y} is ${argv.x * argv.y}`);
+  },
+});
+
+yargs.command({
+  command: "pow",
+  describe: "pow is defined as the result we get on squering some number",
+  builder: {
+    x: {
+      describe: "First Number",
+      demandOption: true,
+      type: "number",
+    },
+  },
+  handler: function (argv) {
+    console.log(`The result of ${argv.x} ^ 2 is ${argv.x * argv.x}`);
+  },
 });
 
 /*
@@ -84,7 +83,5 @@ yargs.command({
 const argv = yargs(process.argv.slice(2)).argv;
 console.log(argv)
 */
-
-
 
 yargs.parse();
