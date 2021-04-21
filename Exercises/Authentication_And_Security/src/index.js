4460ed9bb22783a3b985d652136438726834d585
Sergey <aronovsergeyy@gmail.com>
2021-04-22 01:53:27 +0300

    Authentication_And_Security

---
+++ Exercises/Authentication_And_Security/src/index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+const port = process.env.PORT || 8000;
+
+//Express Middlewares
+app.use(express.json());
+
+//Routes
+// app.use("/api/products", productsRouter);
+
+//Connect to db with mongoose
+mongoose
+  .connect("mongodb://127.0.0.1:27017/authentication", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  })
+  .then(() => {
+    console.log("Database connect");
+  });
+
+//Connected to server
+app.listen(port, () => {
+  console.log("Server listening on port " + port);
+});
